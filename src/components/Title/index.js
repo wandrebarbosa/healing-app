34e915e5bdb52ebe5e21e17933f876b33fe79a91
wandrebarbosa <wandresonbarbosa@gmail.com>
2023-05-23 19:34:50 -0300

    add style in components

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import style from './style';
 
 export default function Title(){
     return(
-        <View>
-            <Text>HEALING - IMC</Text>
+        <View style={style.boxTitle}>
+            <Text style={style.textTitle}>HEALING - IMC</Text>
         </View>
     );
 }
